43ec895b3872d6e0f3449721e3bae286487c97dc
Rudraswamy k <rudratalur@gmail.com>
2021-10-15 15:26:29 +0530

    docs : added files

---
+++ script.js
@@ -0,0 +1,28 @@
+const container = document.querySelector(".container")
+const text = document.querySelector("#text")
+
+
+const totalTime = 15000
+const breatheTime = (totalTime / 5) * 2
+const holdTime = totalTime / 5
+
+breatheAnimation()
+
+function breatheAnimation() {
+    text.innerHTML = "Breath In!"
+    container.className = 'container grow'
+
+    setTimeout(() => {
+        text.innerText = "Hold"
+    
+    setTimeout(() => {
+        text.innerText = "Breathe Out!"
+        container.className = 'container shrink'
+    },holdTime)
+
+
+    }, breatheTime)
+
+}
+
+setInterval (breatheAnimation, totalTime)
